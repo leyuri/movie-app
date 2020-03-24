2077b2e984eb774006f11000b2399172bf61fd83
leyuri <leyuri97@gmail.com>
2020-03-25 03:13:50 +0900

    Add Connect MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://yuri:yuri1123@ds141766.mlab.com:41766/movie', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
 
 app.get('/', (req, res) => res.send('hello world!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

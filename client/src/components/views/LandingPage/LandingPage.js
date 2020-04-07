8f4d361be1d0676ee425d2279ec7bdcdae32dd1a
leyuri <leyuri97@gmail.com>
2020-04-07 23:05:20 +0900

    Set up Proxy

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,13 @@
-import React, { useEffect } from 'react'
+import React,{ useEffect} from 'react'
 import axios from 'axios';
-import { response } from 'express';
 
 function LandingPage() {
 
-
-    useEffect(() => {
-        axios.get('http://localhost:5000/api/hello')
-        .then(response => console.log(response.data))
+     useEffect(() => {
+        axios.get('/api/hello')
+        .then(response=> { console.log(response)})
         //서버에서 돌아오는 console을 보여줄 수 있도록 한 것임
-    }, [])
+     }, [])
 
     return (
         <div>

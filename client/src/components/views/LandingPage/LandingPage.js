92b6b3716403078a8a3c62ad3819e6bc2034fbab
leyuri <leyuri97@gmail.com>
2020-04-08 02:02:43 +0900

    Add Logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,20 +1,36 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
             .then(response => { console.log(response) })
-                    //서버에서 돌아오는 console을 보여줄 수 있도록 한 것임
+                //서버에서 돌아오는 console을 보여줄 수 있도록 한 것임
     }, [])
 
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push("/login")
+                } else {
+                    alert('Failed to logout')
+                }
+            })
+    }
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center'
             , width: '100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+            <br></br><br></br><br></br><br></br>
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
+
         </div>
     )
 }

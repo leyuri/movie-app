3a5bb0a40ef7f5a275245666e551593d1136bfcd
leyuri <leyuri97@gmail.com>
2020-04-08 00:56:48 +0900

    Change LoginPage

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,9 +9,12 @@ function LandingPage() {
         //서버에서 돌아오는 console을 보여줄 수 있도록 한 것임
      }, [])
 
-    return (
-        <div>
-            LandingPage
+     return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <h2>시작 페이지</h2>
         </div>
     )
 }

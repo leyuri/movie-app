665cda95437bf3d094fd3056890111a5f9051bbf
leyuri <leyuri97@gmail.com>
2020-04-21 19:23:08 +0900

    Add API

---
+++ client/src/Config.js
@@ -0,0 +1,5 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '44d6ac37f7a8f90a1dd42aff49669131';
+export  const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
